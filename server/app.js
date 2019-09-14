7070c4be4ba9d0701398bd987a1de8785cf95a86
Rinat <lich666black@yahoo.com>
2019-09-14 15:30:17 +0000

    added ledger ledger-bridge

--- server/app.js
+++ server/app.js
@@ -2,6 +2,21 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
-app.use('/dapp', express.static(path.join(__dirname, '../dapps')))
+const PUBLIC = '/dapp'
+const PUBLIC_DIR = express.static(path.join(__dirname, '../public'))
+
+app.use(PUBLIC, PUBLIC_DIR)
+
+app.get('/ledger-bridge', (req, res, next) => {
+  return res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <body></body>
+    <head>
+      <script src="${PUBLIC}/ledger-bridge.js"></script>
+    </head>
+    </html>
+  `)
+})
 
 module.exports = app
