b0f56a4c91ea1f6642fc54af65c1f9f68b0f8cb8
Rinat <lich666black@yahoo.com>
2019-09-14 15:58:30 +0000

    added 404 page

---
+++ app/pages/_.vue
@@ -0,0 +1,42 @@
+<template>
+  <div :class="b()">
+    <SimpleHeader
+      :title="header.title"
+      :sub-title="header.subTitle"
+    />
+    <div
+      :class="b('separator')"
+    />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import SimpleHeader from '../components/SimpleHeader'
+import Footer from '../views/common/Footer'
+
+export default {
+  name: 'PageNotFound',
+  components: {
+    SimpleHeader,
+    Footer
+  },
+  data () {
+    return {
+      header: {
+        title: 'Page not found',
+        subTitle: ''
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.PageNotFound {
+  &__separator {
+    background-color: $background;
+    height: 27.7vh;
+  }
+}
+</style>
