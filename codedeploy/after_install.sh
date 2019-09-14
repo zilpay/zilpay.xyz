3d040e47808cbe565726c85c2568c9dda3c67156
Rinat <lich666black@yahoo.com>
2019-09-14 19:26:52 +0000

    remove codedeploy

--- codedeploy/after_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# This script is executed after the source is copied to the instances
-cd /var/www/zilpay.xyz
-npm install
--- codedeploy/app_start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# This script is used to start the application
-cd /var/www/zilpay.xyz
-npm start
