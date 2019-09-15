5a2b4c9b56d05c288386a2d8e247d6cdf34f86c5
Rinat <lich666black@yahoo.com>
2019-09-15 15:27:46 +0000

    fixed title

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_title || '',
+    title: process.env.npm_package_title || 'zilpay',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
