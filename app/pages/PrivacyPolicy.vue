ce395173bb658497a6b1058ed9531d61c8b25e31
Rinat <lich666black@gmail.com>
2020-05-26 06:44:50 +0000

    added Privacy policy for cherome

---
+++ app/pages/PrivacyPolicy.vue
@@ -0,0 +1,43 @@
+<template>
+  <div :class="b()">
+    <SimpleHeader :title="header.title" :sub-title="header.subTitle" />
+    <DescriptionList :content-list="list" />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import { uuid } from 'uuidv4'
+
+import SimpleHeader from '@/components/SimpleHeader'
+import DescriptionList from '@/components/DescriptionList'
+import Footer from '@/views/common/Footer'
+
+import PrivacyPolicyData from '@/static/privacy-extension.json'
+
+const list = PrivacyPolicyData.list.map(el => ({
+  ...el,
+  uuidv4: uuid()
+}))
+
+export default {
+  name: 'PrivacyPolicy',
+  components: {
+    SimpleHeader,
+    DescriptionList,
+    Footer
+  },
+  data () {
+    return {
+      list,
+      header: {
+        title: PrivacyPolicyData.title,
+        subTitle: PrivacyPolicyData.subTitle
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
