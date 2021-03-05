19c1bf2701b000490a6c2e6495501b8702dde766
Ринат Хасаншин <rinat@Rinats-MacBook-Pro.local>
2021-03-05 11:29:31 +0500

    fixed doc

--- server/app.js
+++ server/app.js
@@ -2,22 +2,8 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
-const PUBLIC = '/app'
+const DOC_DIST_DIR = express.static(path.join(__dirname, '../dist/Documentation'))
 
-const DOC_DIST_DIR = express.static(path.join(__dirname, '../docs/.vuepress/dist'))
-
-app.use(PUBLIC + '/documentation', DOC_DIST_DIR)
-
-app.get('/ledger-bridge', (req, res, next) => {
-  return res.send(`
-    <!DOCTYPE html>
-    <html lang="en">
-    <body></body>
-    <head>
-      <script src="${PUBLIC}/ledger-bridge.js"></script>
-    </head>
-    </html>
-  `)
-})
+app.use('/documentation', DOC_DIST_DIR)
 
 module.exports = app
